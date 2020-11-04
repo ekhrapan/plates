c949e78bec9486310fb6e44431b260ca519bfb90
ekhrapan <yauheni.khrapan@gmail.com>
2020-11-04 20:58:40 +0300

    code

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,8 +1,25 @@
 package academy.belhard;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Plate p1 = new Plate(5, 2, "asd");
+        Plate p2 = new Plate(5, 2, "asd");
+        Plate p3 = new Plate(5, 3, "asd");
+        Plate p4 = new Plate(11, 4, "big");
+
+        Set<Plate> plates = new HashSet<>();
+        plates.add(p1);
+        plates.add(p2);
+        plates.add(p3);
+        plates.add(p4);
+
+        System.out.println("Количество элементов = " + plates.size());
+        for (Plate plate : plates) {
+            System.out.println(plate);
+        }
     }
 }
